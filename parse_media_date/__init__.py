ab2f842946e59431dcd73ebecc90d2f0ba7ef5b4
Phil Hunt <philiphuntiv2@gmail.com>
2026-03-01 06:22:59 -0500

    Update __init__.py

--- parse_media_date/__init__.py
+++ parse_media_date/__init__.py
@@ -1,15 +1,53 @@
 from philh_myftp_biz.time import from_ymdhms, from_stamp
 from philh_myftp_biz.num import is_int
 from philh_myftp_biz.pc import Path
-from re import split
+from typing import SupportsInt
+from re import split, findall
+
+def _from_ymdhms(
+    year: SupportsInt,
+    month: SupportsInt,
+    day: SupportsInt
+):
+
+    try:
+        
+        stamp = from_ymdhms(
+            year = int(year),
+            month = int(month),
+            day = int(day)
+        )
+
+        return stamp
+
+    except TypeError, ValueError:
+        pass
 
 def parse(file:Path) -> from_stamp:
+
+    #=========================================================================
+
+    iso_parts: list[str] = findall(
+        pattern = r'\d{4}-\d{2}-\d{2}',
+        string = file.name()
+    )
+
+    for part in iso_parts:
+
+        stamp = _from_ymdhms(*part.split('-'))
+
+        if stamp:
+            return stamp
+        
+    #=========================================================================
     
     if file.name().startswith('RPReplay_Final'):
 
         stamp = int(file.name().split('Final')[1])
 
         return from_stamp(stamp)
+    
+    #=========================================================================
 
     parts: list[str] = split(
         pattern = r'[-_\s]',
@@ -22,18 +60,18 @@ def parse(file:Path) -> from_stamp:
         INTEGER = is_int(part)
         
         if CHARCOUNT and INTEGER:
-
-            try:
                 
-                stamp = from_ymdhms(
-                    year = int(part[:4]),
-                    month = int(part[4:6]),
-                    day = int(part[6:8])
-                )
+            stamp = _from_ymdhms(
+                year = part[:4],
+                month = part[4:6],
+                day = part[6:8]
+            )
 
+            if stamp:
                 return stamp
-
-            except TypeError, ValueError:
-                pass
+    
+    #=========================================================================
 
     return file.ctime()
+
+    #=========================================================================
